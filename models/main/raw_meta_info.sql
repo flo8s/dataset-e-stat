d3a60eac45fee4386dd6abdfb35c3af98e7d6d7b
UD <u.yudai1202@gmail.com>
2026-03-25 16:41:03 +0900

    raw モデルの SELECT * を明示的なカラム指定に変更

--- models/main/raw_meta_info.sql
+++ models/main/raw_meta_info.sql
@@ -1,2 +1,12 @@
-SELECT *
+SELECT
+    stats_data_id,
+    class_id,
+    class_name,
+    class_description,
+    item_code,
+    item_name,
+    level,
+    unit,
+    parent_code,
+    add_inf
 FROM {{ source('estat_source', 'meta_info') }}
--- models/main/raw_stats_list.sql
+++ models/main/raw_stats_list.sql
@@ -1,2 +1,2 @@
-SELECT *
+SELECT * EXCLUDE (_dlt_load_id, _dlt_id)
 FROM {{ source('estat_source', 'stats_list') }}
